01521f0bf16c7828ec8faa0690cf00d6d4d5b4a9
John Nemec <john.nemec@rogers.com>
2019-10-01 13:33:43 -0400

    Daily Push
    All frames can now cast abilities, and can now take damage.

--- WarframeTBE/WarframeTBE/WarframeTBE.cpp
+++ WarframeTBE/WarframeTBE/WarframeTBE.cpp
@@ -7,6 +7,14 @@
 using namespace std;
 
 
+//for today
+/*
+	unique frame creation is working
+
+	frames can now take damage
+
+*/
+
 //TODO
 /*
 	Baseline
@@ -18,19 +26,14 @@ using namespace std;
 		Targeting System
 
 	Frames
-		Instance Creation (done)
-
-		Taking Damage
 
 		Bleedout state / Death / Rez tokens
 
 		Abilities
 			All 
-				Instance Creation (done)
 				Functionality 
-				Targeting 
+				Targeting (if an ability targets all enemies, have it target each one instead of a seperate general target all ability funcition)
 				Channel or Cast
-				Cost
 
 			Damage
 				Damage Type
@@ -53,9 +56,8 @@ using namespace std;
 			Other
 
 	Enemies
-		Target Prioritization
 
-		Taking Damage
+		Target Prioritization
 
 		Death
 
@@ -137,13 +139,16 @@ protected:
 	string damageType;
 	//int damageStr[4];  //Leveled Abilities will be done later
 	int damageStr;
-	bool singleTarget;
 public:
 	damageAbility(Ability ability) {
 		abilityName = ability.getName();
 		energyCost = ability.getCost();
 		energyCostTurn = ability.getTurnCost();
 		abilityType = "dmg";
+	}
+	void setAbilityInfo(int damage, string type) {
+		damageStr = damage;
+		damageType = type;
 	}
 
 
@@ -251,27 +256,57 @@ public:
 		sprintSpeed	 = frameSprintSpeed[frameID];
 		passive = passiveAbility(frameAbilityP[frameID]);
 		
-		//gotta do abilities
-		/*
-		if frameID = something then
-			typeability ability1 = typeability(ability from array)
-			same for 2, 3, 4
-
-		that's going to need me to hard code this part for each frame.
-		How else should it be done though? Is there a better way than nested if statements?
-		Let's just hard code one frame and see how it works 
-		isn't there a thing called a switch or something for this?
-		*/
-		if (frameID == 0) {
+		//I still don't like this, but it seems a lot better than the if statements i had before
+		//I wonder if there's still a better way to do it
+		switch (frameID) {
+		case 0: 
 			ability1 = damageAbility(frameAbility1[frameID]);
 			ability2 = buffAbility(frameAbility2[frameID]);
 			ability3 = CCAbility(frameAbility3[frameID]);
 			ability4 = damageAbility(frameAbility4[frameID]);
+		case 1:
+			ability1 = damageAbility(frameAbility1[frameID]);
+			ability2 = buffAbility(frameAbility2[frameID]);
+			ability3 = otherAbility(frameAbility3[frameID]);
+			ability4 = damageAbility(frameAbility4[frameID]);
+		case 2:
+			ability1 = damageAbility(frameAbility1[frameID]);
+			ability2 = CCAbility(frameAbility2[frameID]);
+			ability3 = damageAbility(frameAbility3[frameID]);
+			ability4 = damageAbility(frameAbility4[frameID]);
+		case 3:
+			ability1 = CCAbility(frameAbility1[frameID]);
+			ability2 = buffAbility(frameAbility2[frameID]);
+			ability3 = CCAbility(frameAbility3[frameID]);
+			ability4 = debuffAbility(frameAbility4[frameID]);
+		case 4:
+			ability1 = damageAbility(frameAbility1[frameID]);
+			ability2 = debuffAbility(frameAbility2[frameID]);
+			ability3 = otherAbility(frameAbility3[frameID]);
+			ability4 = damageAbility(frameAbility4[frameID]);
+		case 5:
+			ability1 = otherAbility(frameAbility1[frameID]);
+			ability2 = buffAbility(frameAbility2[frameID]);
+			ability3 = buffAbility(frameAbility3[frameID]);
+			ability4 = CCAbility(frameAbility4[frameID]);
+		case 6:
+			ability1 = debuffAbility(frameAbility1[frameID]);
+			ability2 = debuffAbility(frameAbility2[frameID]);
+			ability3 = buffAbility(frameAbility3[frameID]);
+			ability4 = buffAbility(frameAbility4[frameID]);
+		case 7:
+			ability1 = damageAbility(frameAbility1[frameID]);
+			ability2 = buffAbility(frameAbility2[frameID]);
+			ability3 = otherAbility(frameAbility3[frameID]);
+			ability4 = CCAbility(frameAbility4[frameID]);
 		}
 		
 		
 
 	}
+
+
+	//all my get functions for frames
 	//once again, this doesn't feel too right. Maybe i'm just rusty?
 	string getName() {
 		return Name;
@@ -313,6 +348,9 @@ public:
 	Ability getAbility4() {
 		return ability4;
 	}
+
+
+	//ability functions 
 	bool drainEnergy(int cost) {
 		if (energyCur >= cost) {
 			energyCur -= cost;
@@ -321,7 +359,6 @@ public:
 			return false;
 		}
 	}
-
 	void cast(Ability ability) {
 		if (drainEnergy(ability.getCost())) {
 			ability.cast();
@@ -329,6 +366,25 @@ public:
 			//not enough energy error message	
 			cout << "Not enough energy" << endl;
 		}
+	}
+
+	void getHit(float damage) {
+		//holdup 
+		//armor DR doesn't apply to shields 
+		//whoops
+		
+		if (shieldCur > 0) {
+			shieldCur -= damage;
+			if (shieldCur < 0) {
+				shieldCur = 0;
+			}
+		}
+		else {
+			float DR = (300 / (armor + 300));
+			damage *= DR;
+			healthCur -= damage;
+		}
+		cout << "Frame " << Name << " has " << shieldCur << " shields and " << healthCur <<  " health" << endl;
 	}
 
 };
@@ -362,13 +418,17 @@ public:
 	}
 
 	void getHit(float damage) {
-		//armor dr is 200/  (armor + 200)
-		float DR = (200 / (armor + 200));
-		damage *= DR;
-		cout << damage << endl;
+		//armor DR doesn't apply to shields 
+		
 		if (shieldCur > 0) {
 			shieldCur -= damage;
+			if (shieldCur < 0) {
+				shieldCur = 0;
+			}
+			cout << shieldCur << endl;
 		} else {
+			float DR = (200 / (armor + 200));
+			damage *= DR;
 			healthCur -= damage;
 			cout << healthCur << endl;
 		}
@@ -390,17 +450,33 @@ public:
 	}
 };
 
+class cell {
+protected:
+	Frame frame1;
+	Frame frame2;
+	Frame frame3;
+	Frame frame4;
+
+
+public:
+
+
+};
+
+class enemyCell {};
+
 int main(){
 
-	Enemy test = Enemy(true);
+	
+	for (int i = 0; i < 8; i++) {
+		Frame testF = Frame(i);
+		cout << testF.getName() << endl;
+		testF.cast(testF.getAbility2());
+		testF.getHit(50);
+		testF.getHit(50);
+		testF.getHit(50);
 
-	test.getHit(50);
-
-	Frame testF = Frame(0);
-
-	cout << testF.getName() << endl;
-
-	testF.cast(testF.getAbility4()); //I don't like the function call as an argument for the other function call. Is there a better way?
+	}
 	
 
 	cout << "Welcome to Warframe: Turn Based Edition" << endl;
